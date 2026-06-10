e2bfd6f7917400d8323cd1ddd0fa1d0c69a45792
Mauricio <mnietoll@unsa.edu.pe>
2026-06-10 15:18:10 -0500

    Implementado sort.c

--- sort.c
+++ sort.c
@@ -10,3 +10,25 @@ void swapBytes(void *x, void *y, int size){
         b[i] = tmp;
     }
 }
+
+void insertSorted(void *base, int n, int size, void *x, Comparator cmp){
+    unsigned char *start = base;
+    int i = 0;
+
+    while(cmp(start + i * size, x) <= 0){
+        i++;
+    }
+
+    
+    for(int j = n; j > i; j--){
+        swapBytes(start + j * size, start + (j - 1) * size);
+    }
+}
+
+void insertionSort(void *base, int n, int size, Comparator cmp){
+    if(n == 0 || n == 1)
+        return ;
+
+    insertSorted(base, n - 1, size, base + (n - 1) * size, cmp);
+    insertionSort(base, n - 1, size, cmp);
+}
--- sort.h
+++ sort.h
@@ -3,6 +3,8 @@
 
 typedef int (*Comparator)(const void *, const void *);
 
+void insertSorted(void *, int n, int size, void *, Comparator);
+
 void insertionSort(void *, int, int, Comparator);
 
 void swapBytes(void *, void *, int);
