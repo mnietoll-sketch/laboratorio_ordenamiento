f13e7d9cec5f319f31c539d4ca21e5d98df72e77
Mauricio <mnietoll@unsa.edu.pe>
2026-06-10 14:39:07 -0500

    Creados sort.h y pirate.h

---
+++ main.c
@@ -0,0 +1,7 @@
+#include "sort.h"
+#include <stdio.h>
+
+
+int main(){
+    ;
+}
---
+++ pirate.h
@@ -0,0 +1,15 @@
+#ifndef PIRATE_H
+#define PIRATE_H
+
+#define STRING_SIZE 40
+
+typedef struct{
+    char name[STRING_SIZE];
+    long long int bounty;
+    int age;
+    char crew[STRING_SIZE]:
+} Pirate;
+
+int comparePirates(const void *x, const void *y);
+
+#endif
