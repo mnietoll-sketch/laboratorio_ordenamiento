87460ddd16cafb016c45f2518180acbb639a5a0e
Mauricio <mnietoll@unsa.edu.pe>
2026-06-11 15:16:42 -0500

    completados archivos pirate.h y pirate.c

--- pirate.c
+++ pirate.c
@@ -1,6 +1,30 @@
 #include "pirate.h"
+#include "string.h"
+
 
 int comparePirates(const void *x, const void *y){
-    //TODO: implementar funcion
-    return 0;
+	if(x->bounty > y->bounty)
+		return 1;
+	if(x->bounty < y->bounty)
+		return -1;
+	if(x->age < y->age)
+		return 1;
+	if(x->age > y->age)
+		return -1;
+	if(strcmp(x->name, y->name) < 0)
+		return 1;
+	if(strcmp(x->name, y->name) > 0)
+		return -1;
+	if(strcmp(x->crew, y->crew) < 0)
+		return 1;
+	if(strcmp(x->crew, y->crew) < 0)
+		return -1;
+	return 0;	
+}
+
+void initializePirate(Pirate * p, char *name, long long int bounty, int age, char *crew){
+	strcpy(p->name, name);
+	p->bounty = bounty;
+	p->age = age;
+	strcpy(p->crew, crew);
 }
--- pirate.h
+++ pirate.h
@@ -4,12 +4,14 @@
 #define STRING_SIZE 40
 
 typedef struct{
-    char name[STRING_SIZE];
-    long long int bounty;
-    int age;
-    char crew[STRING_SIZE]:
+	char name[STRING_SIZE];
+	long long int bounty;
+	int age;
+	char crew[STRING_SIZE]:
 } Pirate;
 
 int comparePirates(const void *, const void *);
+
+void initializePirate(Pirate *, char *, long long int, int, char *);
 
 #endif
